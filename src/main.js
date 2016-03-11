5f6eded36222a94e5f8a557de87e92163a5520a8
Hyeon Kim <simnalamburt@gmail.com>
2016-03-11 21:08:29 +0900

    대충 기본 뼈대 만듬

--- src/main.js
+++ src/main.js
@@ -1,13 +1,8 @@
 // @flow
 import React from 'react'
 import ReactDOM from 'react-dom'
-import 'normalize.css/normalize.css'
-import './main.styl'
 
-ReactDOM.render(
-  <div>
-    <h1>Hello, React!</h1>
-    <p>My own react practice!</p>
-  </div>,
-  document.getElementById('example')
-);
+// CSS
+import 'normalize.css/normalize.css'
+import 'github-markdown-css/github-markdown.css'
+import './main.styl'
