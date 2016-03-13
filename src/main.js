226a949c97421a86759aa7c653850a2af716f3d6
Hyeon Kim <simnalamburt@gmail.com>
2016-03-13 20:38:18 +0900

    Contact 정보 따로 분리

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 // @flow
+import contact from './contact.md'
 import header from './header.md'
 
 import 'normalize.css/normalize.css'
@@ -6,4 +7,5 @@ import 'github-markdown-css/github-markdown.css'
 import 'font-awesome/css/font-awesome.css'
 import './main.styl'
 
+document.getElementById('contact').innerHTML = contact;
 document.getElementById('header').innerHTML = header;
