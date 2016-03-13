e90038709f3bae05f5a86705844447b1c9cd31b6
Hyeon Kim <simnalamburt@gmail.com>
2016-03-13 19:36:15 +0900

    font-awesome 설치

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import header from './header.md'
 
 import 'normalize.css/normalize.css'
 import 'github-markdown-css/github-markdown.css'
+import 'font-awesome/css/font-awesome.css'
 import './main.styl'
 
 document.getElementById('header').innerHTML = header;
