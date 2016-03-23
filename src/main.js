585e3805fba0216cbdca42e22410ed1b2c389673
Hyeon Kim <simnalamburt@gmail.com>
2016-03-24 04:43:59 +0900

    빌드 결과 경로를 html에 박는건 안좋은거같음

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
 // @flow
 import header from './header.md'
 import main from './main.md'
+import profile from './img/profile.png'
 
 import 'normalize.css/normalize.css'
 import 'github-markdown-css/github-markdown.css'
 import 'font-awesome/css/font-awesome.css'
 import './main.styl'
-import './img/profile.png'
 
+document.getElementById('me').src = profile;
 document.getElementById('header').innerHTML = header;
 document.getElementById('main').innerHTML = main;
