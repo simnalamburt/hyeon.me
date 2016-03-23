dc6523625b884752825c52b38166b05f42389f52
Hyeon Kim <simnalamburt@gmail.com>
2016-03-24 04:13:40 +0900

    프사 이미지 작은것 사용

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import 'normalize.css/normalize.css'
 import 'github-markdown-css/github-markdown.css'
 import 'font-awesome/css/font-awesome.css'
 import './main.styl'
+import './img/profile.png'
 
 document.getElementById('header').innerHTML = header;
 document.getElementById('main').innerHTML = main;
