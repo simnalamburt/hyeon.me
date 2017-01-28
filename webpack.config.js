a93714d5bdb04cb4871ceaa8d802c06d0b08f1b2
Hyeon Kim <simnalamburt@gmail.com>
2017-01-28 22:19:43 +0900

    Update webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,53 @@
+'use strict';
+const path = require('path');
 const webpack = require('webpack');
 const autoprefixer = require('autoprefixer');
-
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const failPlugin = require('webpack-fail-plugin');
+
 
 // Always enabled plugins
-const plugs = [
-  new ExtractTextPlugin('_bundle.css')
+let plugins = [
+  // Extract CSS files to the 'bundle.css'.
+  new ExtractTextPlugin('_bundle.css'),
+  // This plugin should be always required. See https://github.com/webpack/webpack/issues/708
+  failPlugin,
 ];
 
 // Production only plugins
-const prod = [
-  new webpack.DefinePlugin({ 'process.env': { NODE_ENV: JSON.stringify('production') } })
-];
+if (process.env.NODE_ENV === 'production') {
+  plugins = plugins.concat([
+    // Pass the 'NODE_ENV=production' environment variable to the child processes.
+    new webpack.DefinePlugin({ 'process.env': { NODE_ENV: JSON.stringify('production') } }),
+    // Minimize the output
+    new webpack.optimize.UglifyJsPlugin({ compress: { warnings: false } }),
+  ]);
+}
+
 
 module.exports = {
-  context: `${__dirname}/src`,
   entry: './main.js',
+  context: path.resolve(__dirname, 'src'),
   output: {
+    filename: '_bundle.js',
     path: `${__dirname}/public/build`,
     publicPath: '/build/',
-    filename: '_bundle.js'
   },
-  devtool: 'source-map',
-  plugins: process.env.NODE_ENV !== 'production' ? plugs : plugs.concat(prod),
   module: {
     loaders: [
       { test: /\.md$/, loader: 'html!markdown' },
-      { test: /\.(?:png|jpg)$/, loader: 'file?name=static/[hash].[ext]' },
-      { test: /\.(?:woff2?|ttf|eot|svg)(?:\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file?name=static/[hash].[ext]' },
+      { test: /\.(?:png|jpg|(?:woff2?|ttf|eot|svg)(?:\?v=[0-9]\.[0-9]\.[0-9])?)$/, loader: 'file?name=static/[hash].[ext]' },
       { test: /\.css$/, loader: ExtractTextPlugin.extract('style', 'css') },
       { test: /\.styl$/, loader: ExtractTextPlugin.extract('style', 'css!postcss!stylus') },
       {
-        test: /\.jsx?$/,
+        test: /\.js$/,
         exclude: /node_modules/,
         loader: 'babel',
-        query: { presets: ['es2015', 'stage-3', 'react'] }
+        query: { presets: ['latest'] }
       }
     ]
   },
-  postcss: () => [autoprefixer]
+  plugins,
+  devtool: 'source-map',
+  postcss: _ => [autoprefixer]
 };
