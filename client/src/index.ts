7bd330bbd3bbf666d5f343dd9a4f1f02e389cf1a
Hyeon Kim <simnalamburt@gmail.com>
2017-05-06 18:55:21 +0900

    Update main.js into index.ts

--- client/src/index.ts
+++ client/src/index.ts
@@ -2,12 +2,13 @@ import header from './header.md'
 import main from './main.md'
 import profile from './img/profile.jpg'
 
+// Stylesheets
 import 'normalize.css/normalize.css'
 import 'github-markdown-css/github-markdown.css'
 import 'font-awesome/css/font-awesome.css'
 import './main.styl'
 import './analytics.js'
 
-document.getElementById('me').src = profile;
-document.getElementById('header').innerHTML = header;
-document.getElementById('main').innerHTML = main;
+document.getElementById('me').src = profile
+document.getElementById('header').innerHTML = header
+document.getElementById('main').innerHTML = main
