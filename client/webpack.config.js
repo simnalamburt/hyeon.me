e515582dd41570e569eb3fd987014016b27e0b00
Hyeon Kim <simnalamburt@gmail.com>
2017-05-06 18:25:40 +0900

    Completely rewrite webpack.config.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,53 +1,71 @@
-'use strict';
-const path = require('path');
-const webpack = require('webpack');
-const autoprefixer = require('autoprefixer');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const failPlugin = require('webpack-fail-plugin');
+'use strict'
 
+const path = require('path')
+const webpack = require('webpack')
+const merge = require('webpack-merge')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CompressionPlugin = require('compression-webpack-plugin')
+const { CheckerPlugin } = require('awesome-typescript-loader')
 
-// Always enabled plugins
-let plugins = [
-  // Extract CSS files to the 'bundle.css'.
-  new ExtractTextPlugin('_bundle.css'),
-  // This plugin should be always required. See https://github.com/webpack/webpack/issues/708
-  failPlugin,
-];
-
-// Production only plugins
-if (process.env.NODE_ENV === 'production') {
-  plugins = plugins.concat([
-    // Pass the 'NODE_ENV=production' environment variable to the child processes.
-    new webpack.DefinePlugin({ 'process.env': { NODE_ENV: JSON.stringify('production') } }),
-    // Minimize the output
-    new webpack.optimize.UglifyJsPlugin({ compress: { warnings: false } }),
-  ]);
-}
-
-
-module.exports = {
-  entry: './main.js',
-  context: path.resolve(__dirname, 'src'),
+//
+// Common configs
+//
+const commonConfigs = {
+  entry: './src/index.ts',
   output: {
-    filename: '_bundle.js',
-    path: `${__dirname}/public/build`,
-    publicPath: '/build/',
+    filename: '[hash].js',
+    path: path.resolve(__dirname, '../dist')
   },
   module: {
-    loaders: [
-      { test: /\.md$/, loader: 'html!markdown' },
-      { test: /\.(?:png|jpg|(?:woff2?|ttf|eot|svg)(?:\?v=[0-9]\.[0-9]\.[0-9])?)$/, loader: 'file?name=static/[hash].[ext]' },
-      { test: /\.css$/, loader: ExtractTextPlugin.extract('style', 'css') },
-      { test: /\.styl$/, loader: ExtractTextPlugin.extract('style', 'css!postcss!stylus') },
+    rules: [
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel',
-        query: { presets: ['latest'] }
+        test: /\.md$/, use: [
+          { loader: 'html-loader' },
+          { loader: 'markdown-loader' },
+        ]
+      },
+      { test: /\.html$/, use: 'html-loader' },
+      { test: /\.ts$/, use: 'awesome-typescript-loader' },
+      {
+        test: /\.(?:jpg|png|(?:woff2?|ttf|eot|svg)(?:\?v=[0-9]\.[0-9]\.[0-9])?)$/,
+        use: 'file-loader?name=[hash].[ext]'
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract(['css-loader', 'postcss-loader'])
       }
     ]
   },
-  plugins,
-  devtool: 'source-map',
-  postcss: _ => [autoprefixer]
-};
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  plugins: [
+    new ExtractTextPlugin('[hash].css'),
+    new HtmlWebpackPlugin({ template: 'src/index.html' }),
+    new CheckerPlugin(),
+  ],
+}
+
+//
+// Development-mode configs
+//
+const devConfigs = {
+  devtool: 'source-map'
+}
+
+//
+// Production-mode configs
+//
+const productionConfigs = {
+  plugins: [
+    new webpack.LoaderOptionsPlugin({ minimize: true, debug: false }),
+    new webpack.optimize.UglifyJsPlugin(),
+    new webpack.DefinePlugin({ 'process.env.NODE_ENV': JSON.stringify('production') }),
+    new CompressionPlugin({ test: /\.(?:css|js|svg|eot|ttf|html)$/ }),
+  ]
+}
+
+module.exports = env => env === 'production' ?
+  merge(commonConfigs, productionConfigs) :
+  merge(commonConfigs, devConfigs)
