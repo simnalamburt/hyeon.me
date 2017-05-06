f3ebee05caa57fe1453a3ccbc07910412f509209
Hyeon Kim <simnalamburt@gmail.com>
2017-05-06 18:55:33 +0900

    Update main.styl into index.css

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,13 +1,13 @@
 import header from './header.md'
 import main from './main.md'
 import profile from './img/profile.jpg'
+import './analytics.js'
 
 // Stylesheets
 import 'normalize.css/normalize.css'
 import 'github-markdown-css/github-markdown.css'
 import 'font-awesome/css/font-awesome.css'
-import './main.styl'
-import './analytics.js'
+import './index.css'
 
 document.getElementById('me').src = profile
 document.getElementById('header').innerHTML = header
