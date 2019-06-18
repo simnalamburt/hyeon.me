40e7598184e4eaa96b04b0651b496e1c73936fa2
Hyeon Kim <simnalamburt@gmail.com>
2019-06-19 02:17:42 +0900

    Get rid of Facebook Pixel

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,6 @@
 import header from './header.md'
 import main from './main.md'
 import profile from './img/profile.jpg'
-import './analytics.js'
 
 // Stylesheets
 import 'normalize.css/normalize.css'
@@ -9,6 +8,16 @@ import 'github-markdown-css/github-markdown.css'
 import 'font-awesome/css/font-awesome.css'
 import './index.scss'
 
+// Render markdown
 document.getElementById('me').src = profile
 document.getElementById('header').innerHTML = header
 document.getElementById('main').innerHTML = main
+
+// Google Analytics
+// prettier-ignore
+;(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga')
+ga('create', 'UA-47724944-1', 'auto')
+ga('send', 'pageview')
