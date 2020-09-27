307be9c885bd5008831aaaf6f24a33928db0925d
Hyeon Kim <simnalamburt@gmail.com>
2020-09-28 05:22:29 +0900

    Update google analytics

--- client/src/index.js
+++ client/src/index.js
@@ -12,12 +12,3 @@ import './index.scss'
 document.getElementById('me').src = profile
 document.getElementById('header').innerHTML = header
 document.getElementById('main').innerHTML = main
-
-// Google Analytics
-// prettier-ignore
-;(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga')
-ga('create', 'UA-47724944-1', 'auto')
-ga('send', 'pageview')
