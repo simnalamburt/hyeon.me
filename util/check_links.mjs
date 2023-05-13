e6b4122f02c989b222a46eb5df2ff8f620cb375f
Hyeon Kim <simnalamburt@gmail.com>
2023-05-14 00:16:55 +0900

    Replace twitter-text with marked, html5parser

--- util/check_links.mjs
+++ util/check_links.mjs
@@ -1,5 +1,6 @@
 import { readFile } from 'node:fs/promises'
-import twitter from 'twitter-text'
+import { parse as compile } from 'marked'
+import { parse, walk } from 'html5parser'
 
 const reset = '\x1b[0m'
 const red = '\x1b[31m'
@@ -7,8 +8,31 @@ const green = '\x1b[32m'
 const yellow = '\x1b[33m'
 
 const filePath = new URL('../src/main.md', import.meta.url)
-const contents = await readFile(filePath, { encoding: 'utf8' })
-const urls = twitter.extractUrls(contents, /https?/)
+const markdown = await readFile(filePath, { encoding: 'utf8' })
+const html = compile(markdown)
+const ast = parse(html, { setAttributeMap: true })
+
+const urls = new Set()
+walk(ast, {
+  enter: (node) => {
+    let url
+    switch (node.name) {
+      case 'a':
+        url = node.attributeMap.href.value.value
+        break
+      case 'img':
+        url = node.attributeMap.src.value.value
+        break
+      default:
+        return
+    }
+    if (!/https?:\/\//.test(url)) {
+      return
+    }
+    urls.add(url)
+  },
+})
+
 for (const url of urls) {
   forEachUrl(url)
 }
